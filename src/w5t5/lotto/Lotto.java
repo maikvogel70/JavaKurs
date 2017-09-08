631f9cd4a8a39ffd22c13f9bc1b06a3da819c481
Alfa <Alfa@MD-Home-003>
2017-09-08 15:34:02 +0200

    lotto

---
+++ src/w5t5/lotto/Lotto.java
@@ -0,0 +1,17 @@
+package w5t5.lotto;
+
+import javax.swing.JFrame;
+
+public class Lotto extends JFrame {
+
+	public static void main(String[] args) {
+		Lotto lotto = new Lotto();
+		lotto.init();
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
